d21c6157d88f295ee70dfac322a1b9f88e0d78f6
Ilya Dzemuanets <hirami14590@gamil.com>
2022-03-22 16:18:52 +0300

    Add service interfaces

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -1,13 +1,21 @@
 package service
 
 import (
+	"context"
 	"os"
 	"time"
 
 	"github.com/Hargeon/vidbot/pkg/service/videocmprs"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 type Bot interface {
+}
+
+type MessageHandler interface {
+	Execute(int64, *videocmprs.Account, tgbotapi.Update)
+	SetNext(MessageHandler)
 }
 
 type Cache interface {
@@ -18,6 +26,10 @@ type Cache interface {
 
 type VideoService interface {
 	Authenticate(chatID int64) (string, error)
-	Registration(chatID int64, token string) error
+	Registration(chatID int64, email string) error
 	SendVideo(file *os.File, tokenAuth string, req *videocmprs.Request) error
+}
+
+type CloudStorage interface {
+	Download(ctx context.Context, id string) (string, error)
 }
